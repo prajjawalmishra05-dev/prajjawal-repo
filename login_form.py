8f7f5cc03c5b77ad78d792bc7fe6dd450927ed62
PRAJJAWAL-REPO <prajjawalmishra05@gmail.com>
2026-02-16 23:36:18 +0530

    this is my first change in my login form

--- login_form.py
+++ login_form.py
@@ -6,7 +6,7 @@ root.title('Login form')
 lbl1= Label(root,text="Login form",font=('Arial',20),fg='red')
 lbl1.place(x=30,y=30)
 
-userlbl=Label(root,text ='user name')
+userlbl=Label(root,text ='prajjawal')
 userlbl.place(x=40,y=70)
 
 usertxt=Entry()
